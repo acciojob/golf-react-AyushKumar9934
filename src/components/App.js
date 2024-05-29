149c00c25b56c42e261bb7a847c12261ed88065e
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-05-29 17:24:30 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -11,23 +11,52 @@ class App extends Component {
         };
         this.renderChoice = this.renderBallOrButton.bind(this)
         this.buttonClickHandler = this.buttonClickHandler.bind(this)
+        this.handleKeyPress = this.handleKeyPress.bind(this);
     };
 
     buttonClickHandler() {
+
+        this.setState(prevState=>({
+            renderBall:true
+          }))
    
    }
     renderBallOrButton() {
 		if (this.state.renderBall) {
 		    return <div className="ball" style={this.state.ballPosition}></div>
 		} else {
-		    return <button onClick={this.buttonClickHandler} >Start</button>
+		    return <button className="start" onClick={this.buttonClickHandler} >Start</button>
 		}
     }
 
     // bind ArrowRight keydown event
-    componentDidMount() {
-      
+   // addEventListener("keydown", myScript)
+   handleKeyPress(event){
+    if(event.key==="ArrowRight"){
+        this.setState(prevState => {
+            let newPosi = prevState.posi + 5;
+            return {
+                posi: newPosi,
+                ballPosition: { left: `${newPosi}px` }
+            }
+        });
     }
+}
+//    handleKeyPress = (event) => {
+//     if (event.key === "ArrowRight") {
+//         this.setState(prevState => ({
+//             posi: prevState.posi + 5,
+//             ballPosition: { left: `${prevState.posi + 5}px` }
+//         }));
+//     }
+// }
+componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyPress);
+}
+   
+   componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyPress);
+}
 
     render() {
         return (
